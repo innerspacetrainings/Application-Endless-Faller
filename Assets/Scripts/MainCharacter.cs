d81c19276c19b645f2fcb67a716aa0fff9db2cea
Veaceslav <58423466+zevsmdd@users.noreply.github.com>
2020-09-12 22:31:32 +0200

    Implemented physical movement of the cube
    Aproximately 1.5 hrs including testing settings to get a good feel of the cube

--- Assets/Scripts/MainCharacter.cs
+++ Assets/Scripts/MainCharacter.cs
@@ -1,24 +1,41 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class MainCharacter : MonoBehaviour
 {
-    [SerializeField] private float speed;
+    Rigidbody rb;
+    float input;
+    bool touchedWall;
+    ParticleSystem trailParticles;
+
+    [SerializeField] float speed, force;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        trailParticles = transform.GetChild(0).GetComponent<ParticleSystem>();       
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.LeftArrow))
+        input = Input.GetAxis("Horizontal");
+
+        if (input != 0 && !touchedWall)
         {
-            transform.position += Vector3.left * speed;
-        } else if (Input.GetKey(KeyCode.RightArrow))
-        {
-            transform.position += Vector3.right * speed;
+            rb.angularVelocity = Vector3.back * (input * speed);
+            rb.AddForce(Vector3.right * force * input, ForceMode.Impulse);
         }
+
+        if (rb.velocity.y > 2)
+            trailParticles.Play();
+        else
+            trailParticles.Stop();
+    }
+
+    void OnCollisionStay(Collision other)
+    {
+        if (other.transform.CompareTag("Wall"))
+            touchedWall = true;
+        else
+            touchedWall = false;
     }
 }
